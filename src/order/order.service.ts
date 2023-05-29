7978ec43999e99337071c13a99d1fd00f1291622
Huỳnh Kim Hưng <infernoid0410@gmail.com>
2023-05-29 16:24:44 +0700

    Modify Product Recommendation

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -8,6 +8,7 @@ import { OrderDocument, Order } from '../model/order.schema';
 import { CreateOrderInput, UpdateOrderInput } from '../dto/order.dto';
 import { PaymentDocument, Payment } from '../model/payment.schema';
 import { createPaymentInput } from '../dto/payment.dto';
+import { ObjectId } from 'mongodb';
 interface Itemset {
   items: string[];
   support?: number;
@@ -129,83 +130,173 @@ export class OrderService {
     }
   }
 
+  // async userRecommendation(user: string) {
+  //   try {
+  //     const orders = await this.orderModel.find();
+  //     const users = Array.from(new Set(orders.map((order) => order.user)));
+  //     const products = Array.from(
+  //       new Set(
+  //         orders.flatMap((order) => order.cart.map((product) => product.id)),
+  //       ),
+  //     );
+  //     const userItemMatrix: number[][] = [];
+  //     for (const user of users) {
+  //       const userRow = new Array(products.length).fill(0);
+  //       for (const order of orders.filter((order) => order.user === user)) {
+  //         for (const product of order.cart) {
+  //           const productIndex = products.indexOf(product.id);
+  //           userRow[productIndex] += 1;
+  //         }
+  //       }
+  //       userItemMatrix.push(userRow);
+  //     }
+
+  //     // compute similarities
+  //     const similarities: number[][] = [];
+  //     for (let i = 0; i < users.length; i++) {
+  //       const similarityRow = new Array(users.length).fill(0);
+  //       for (let j = 0; j < users.length; j++) {
+  //         const user1 = userItemMatrix[i];
+  //         const user2 = userItemMatrix[j];
+  //         const similarity = cosineSimilarity(user1, user2);
+  //         similarityRow[j] = similarity;
+  //       }
+  //       similarities.push(similarityRow);
+  //     }
+
+  //     function cosineSimilarity(u: number[], v: number[]): number {
+  //       const dotProduct = u.reduce((sum, _, i) => sum + u[i] * v[i], 0);
+  //       const uMagnitude = Math.sqrt(u.reduce((sum, x) => sum + x * x, 0));
+  //       const vMagnitude = Math.sqrt(v.reduce((sum, x) => sum + x * x, 0));
+  //       return dotProduct / (uMagnitude * vMagnitude);
+  //     }
+  //     console.log(users, user);
+  //     const userIndex = users.indexOf(user);
+  //     if (userIndex === -1) {
+  //       // user not found in orders, return empty list of recommendations
+  //       return [];
+  //     }
+  //     const similarUsers: string[] = [];
+  //     for (let i = 0; i < users.length; i++) {
+  //       if (i !== userIndex && similarities[userIndex][i] > 0) {
+  //         similarUsers.push(users[i]);
+  //       }
+  //     }
+  //     console.log(userIndex, similarUsers);
+  //     const recommendedProducts = new Set<string>();
+  //     for (const similarUser of similarUsers) {
+  //       const similarUserIndex = users.indexOf(similarUser);
+  //       const similarUserRow = userItemMatrix[similarUserIndex];
+  //       for (let i = 0; i < similarUserRow.length; i++) {
+  //         if (similarUserRow[i] > 0 && userItemMatrix[userIndex][i] === 0) {
+  //           recommendedProducts.add(products[i]);
+  //           if (recommendedProducts.size === 6) {
+  //             // stop adding more recommended products
+  //             break;
+  //           }
+  //         }
+  //       }
+  //       if (recommendedProducts.size === 6) {
+  //         // stop iterating over similar users if we already have 6 recommendations
+  //         break;
+  //       }
+  //     }
+  //     return Array.from(recommendedProducts);
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }
   async userRecommendation(user: string) {
     try {
       const orders = await this.orderModel.find();
       const users = Array.from(new Set(orders.map((order) => order.user)));
-      const products = Array.from(
-        new Set(
-          orders.flatMap((order) => order.cart.map((product) => product.id)),
-        ),
-      );
-      const userItemMatrix: number[][] = [];
+
+      // const products = Array.from(
+      //   new Set(
+      //     orders.flatMap((order) => order.cart.map((product) => product.id)),
+      //   ),
+      // );
+
+      const userItemMatrix: Map<string, Map<string, number>> = new Map();
+
       for (const user of users) {
-        const userRow = new Array(products.length).fill(0);
-        for (const order of orders.filter((order) => order.user === user)) {
-          for (const product of order.cart) {
-            const productIndex = products.indexOf(product.id);
-            userRow[productIndex] += 1;
+        userItemMatrix.set(String(user), new Map());
+      }
+
+      for (const order of orders) {
+        const currentUserItems = userItemMatrix.get(String(order.user));
+
+        for (const product of order.cart) {
+          const productId = product.id;
+
+          if (!currentUserItems.has(productId)) {
+            currentUserItems.set(productId, 0);
           }
+
+          currentUserItems.set(productId, currentUserItems.get(productId) + 1);
         }
-        userItemMatrix.push(userRow);
+        // console.log('after', currentUserItems);
       }
 
-      // compute similarities
-      const similarities: number[][] = [];
-      for (let i = 0; i < users.length; i++) {
-        const similarityRow = new Array(users.length).fill(0);
-        for (let j = 0; j < users.length; j++) {
-          const user1 = userItemMatrix[i];
-          const user2 = userItemMatrix[j];
-          const similarity = cosineSimilarity(user1, user2);
-          similarityRow[j] = similarity;
-        }
-        similarities.push(similarityRow);
-      }
-
-      function cosineSimilarity(u: number[], v: number[]): number {
-        const dotProduct = u.reduce((sum, _, i) => sum + u[i] * v[i], 0);
-        const uMagnitude = Math.sqrt(u.reduce((sum, x) => sum + x * x, 0));
-        const vMagnitude = Math.sqrt(v.reduce((sum, x) => sum + x * x, 0));
-        return dotProduct / (uMagnitude * vMagnitude);
-      }
-      console.log(users, user);
-      const userIndex = users.indexOf(user);
-      if (userIndex === -1) {
-        // user not found in orders, return empty list of recommendations
+      const targetUserItems = userItemMatrix.get(user);
+      if (!targetUserItems) {
         return [];
       }
-      const similarUsers: string[] = [];
-      for (let i = 0; i < users.length; i++) {
-        if (i !== userIndex && similarities[userIndex][i] > 0) {
-          similarUsers.push(users[i]);
+
+      const similarUsers: Map<string, number> = new Map();
+
+      for (const [otherUser, items] of userItemMatrix.entries()) {
+        if (otherUser !== user) {
+          let intersectionCount = 0;
+
+          for (const [productId] of targetUserItems) {
+            if (items.has(productId)) {
+              intersectionCount++;
+            }
+          }
+
+          const similarity =
+            intersectionCount / Math.sqrt(targetUserItems.size * items.size);
+
+          if (similarity > 0) {
+            similarUsers.set(otherUser, similarity);
+          }
         }
       }
-      console.log(userIndex, similarUsers);
-      const recommendedProducts = new Set<string>();
-      for (const similarUser of similarUsers) {
-        const similarUserIndex = users.indexOf(similarUser);
-        const similarUserRow = userItemMatrix[similarUserIndex];
-        for (let i = 0; i < similarUserRow.length; i++) {
-          if (similarUserRow[i] > 0 && userItemMatrix[userIndex][i] === 0) {
-            recommendedProducts.add(products[i]);
+
+      const sortedSimilarUsers = Array.from(similarUsers.entries()).sort(
+        (a, b) => b[1] - a[1],
+      );
+      const recommendedProducts: Set<string> = new Set();
+
+      for (const [otherUser, similarity] of sortedSimilarUsers) {
+        const otherUserItems = userItemMatrix.get(otherUser);
+
+        for (const [productId] of otherUserItems) {
+          if (!targetUserItems.has(productId)) {
+            recommendedProducts.add(productId);
+
             if (recommendedProducts.size === 6) {
-              // stop adding more recommended products
+              // Stop adding more recommended products
               break;
             }
           }
         }
+
         if (recommendedProducts.size === 6) {
-          // stop iterating over similar users if we already have 6 recommendations
+          // Stop iterating over similar users if we already have 6 recommendations
           break;
         }
       }
       return Array.from(recommendedProducts);
     } catch (error) {
-      console.log(error);
-      throw error;
+      // Handle any errors
+      console.error(error);
+      return [];
     }
   }
+
   async createPayment(createPaymentInput: createPaymentInput) {
     try {
       const newPayment = await this.paymentModel.create(createPaymentInput);
